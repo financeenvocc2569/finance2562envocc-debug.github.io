ef061fae0561896175912ebcf87206a4de651eab
finance2562envocc-debug <finance2562.envocc@gmail.com>
2026-03-19 07:53:40 +0700

    +

--- config.js
+++ config.js
@@ -1,29 +1,8 @@
-/* global window */
 (function (global) {
   'use strict';
 
   global.DOC_CONTROL_CONFIG = {
-    appName: 'ระบบทะเบียนคุมเอกสาร',
-    subtitle: 'พร้อมใช้งานกับ Apps Script API',
-    themePreset: 'apple-glass',
-
-    // จำเป็น: ใส่ URL Web App ที่ลงท้ายด้วย /exec
-    scriptUrl: 'https://script.google.com/macros/s/AKfycby_MxLyValLzYNgtgsl903MIm0eAs7Bh6zZLBGE4kC9iCGugMib5RYEfDZsevIu_hM/exec',
-
-    // ไม่จำเป็น: ถ้าเว้นว่าง ระบบจะสร้างและจำค่าให้เองในเบราว์เซอร์
-    deviceKey: '',
-
-    // ถ้า true จะซ่อนปุ่มตั้งค่า API บนหน้าเว็บ และบังคับใช้ค่าจากไฟล์นี้
-    lockSettings: true,
-    requestTimeoutMs: 22000,
-
-    // ใส่เฉพาะลิงก์ที่ต้องการแสดงบนหน้าเว็บ
-    links: {
-      webApp: '',
-      spreadsheet: '',
-      appsScriptProject: '',
-      driveFolder: '',
-      manual: ''
-    }
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbz1jajW3BCP1XBFCjgOdg440WooyoBMmtfdcrFUk2OINl0DzZhVfa1BqkF9Uquhh6E/exec',
+    lockSettings: true
   };
 })(typeof window !== 'undefined' ? window : this);
